cba111a9e26a4901775672210258d185fec5fae1
fuyqz <112553934+fuyqz@users.noreply.github.com>
2022-12-15 12:12:18 +0800

    Update env.js

--- xinhainet/uniCloud-aliyun/cloudfunctions/fun/env.js
+++ xinhainet/uniCloud-aliyun/cloudfunctions/fun/env.js
@@ -1,7 +1,8 @@
 module.exports = {
-    ADMIN_SECRET: "suibianyiduanzifu",
-    WX_APPID: "	wx6e0d199766cfba3a",
-    WX_APPSECRET: "13f05fa6717d35750228331d461a87a8",
+    //填自己的
+    ADMIN_SECRET: "xxx",
+    WX_APPID: "	xxx",
+    WX_APPSECRET: "xxx",
     WX_MCHID: "微信支付商户号",
     WX_PARTERNER_KEY: "微信支付parterner秘钥",
     WX_PAYURL: "你的接收支付通知的函数url地址",
